1656d141d4765c16fff68f2a8a4fb59a09431062
peter oluwafemi agboola <107320758+P-agboola@users.noreply.github.com>
2023-06-01 14:39:56 +0100

    profile service spec test done

--- src/profiles/profiles.controller.spec.ts
+++ src/profiles/profiles.controller.spec.ts
@@ -1,12 +1,120 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ProfilesController } from './profiles.controller';
+import { ProfilesService } from './profiles.service';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
+import { Profile } from './profile.entity';
+import { NotFoundException } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { User } from '../users/entities/user.entity';
 
 describe('ProfilesController', () => {
   let controller: ProfilesController;
+  const testProfile: CreateProfileDto = {
+    firstName: 'John',
+    lastName: 'Doe',
+    gender: 'male',
+    occupation: 'Software Engineer',
+    phoneNumber: '1234567890',
+    email: 'johndoe@example.com',
+  };
+  const updateTestProfile: UpdateProfileDto = {
+    occupation: 'Software Engineer',
+    phoneNumber: '1234567890',
+  };
+  const user: User = {
+    id: 1,
+    userName: 'johndoe',
+    email: 'johndoe@example.com',
+    profile: null,
+    tasks: [],
+    hasId: () => true, // Assuming these methods exist in the User entity
+    save: () => Promise.resolve(user),
+    remove: () => Promise.resolve(user),
+    softRemove: () => Promise.resolve(user),
+    recover: () => Promise.resolve(user),
+    reload: () => Promise.resolve(),
+  };
+  const profile: Profile = {
+    id: 1,
+    ...testProfile,
+    user: user,
+  };
+  const mockUserRepository = {
+    findOne: jest.fn((query) => {
+      if (query.where.email === user.email) {
+        return Promise.resolve(user);
+      }
+      return Promise.resolve(null); // Return null when email doesn't match
+    }),
+  };
+  const mockProfileService = {
+    createProfile: jest.fn(async (createProfile: CreateProfileDto) => {
+      console.log('Mock createProfile called with:', createProfile);
+      await Promise.resolve(
+        mockUserRepository.findOne({ where: { email: createProfile.email } }),
+      );
+      console.log('Mock createProfile is being called');
+      return Promise.resolve({
+        id: 1,
+        ...createProfile,
+        user: user,
+      });
+    }),
+
+    getProfileById: jest.fn((id: number) => {
+      if (id === profile.id) {
+        return Promise.resolve(profile);
+      }
+      throw new NotFoundException('Profile id not found');
+    }),
+
+    getAllProfiles: jest.fn(() => {
+      return Promise.resolve([profile]);
+    }),
+
+    updateProfile: jest.fn((id: number, updateProfile: UpdateProfileDto) => {
+      if (id === profile.id) {
+        return Promise.resolve({ id: profile.id, ...updateProfile });
+      }
+      throw new NotFoundException('Profile id not found');
+    }),
+
+    deleteProfile: jest.fn((id: number) => {
+      if (id === profile.id) {
+        return Promise.resolve('Profile deleted');
+      }
+      throw new NotFoundException('Profile id not found');
+    }),
+  };
+
+  // const mockRepository = {
+  //   findOne: jest.fn((query: any) => {
+  //     return Promise.resolve(query);
+  //   }),
+
+  //   find: jest.fn(() => {
+  //     return Promise.resolve([profile]);
+  //   }),
+  // findOneBy: jest.fn((query: any) => {
+  //   return Promise.resolve(query);
+  // }),
+  // };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ProfilesController],
+      providers: [
+        ProfilesService,
+        {
+          provide: getRepositoryToken(Profile),
+          useValue: mockProfileService,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useValue: mockUserRepository,
+        },
+      ],
     }).compile();
 
     controller = module.get<ProfilesController>(ProfilesController);
@@ -15,4 +123,62 @@ describe('ProfilesController', () => {
   it('should be defined', () => {
     expect(controller).toBeDefined();
   });
+
+  it('should create a profile', async () => {
+    const createdProfile = await controller.createProfile(testProfile);
+    expect(createdProfile).toEqual({ id: 1, ...testProfile, user: user });
+    expect(mockProfileService.createProfile).toHaveBeenCalledWith(testProfile);
+    console.log('Controller createProfile method called');
+  });
+
+  // it('should get a profile by id', async () => {
+  //   const retrievedProfile = await controller.getProfilesById(1);
+  //   expect(retrievedProfile).toEqual(profile);
+  //   expect(mockProfileService.getProfileById).toHaveBeenCalledWith(1);
+  // });
+
+  // it('should throw NotFoundException when getting a non-existent profile by id', async () => {
+  //   await expect(controller.getProfilesById(2)).rejects.toThrowError(
+  //     NotFoundException,
+  //   );
+  //   expect(mockProfileService.getProfileById).toHaveBeenCalledWith(2);
+  // });
+
+  // it('should get all profiles', async () => {
+  //   const allProfiles = await controller.getAllProfiles();
+  //   expect(allProfiles).toEqual([profile]);
+  //   expect(mockProfileService.getAllProfiles).toHaveBeenCalled();
+  // });
+
+  // it('should update a profile', async () => {
+  //   const updatedProfile = await controller.updateProfile(1, updateTestProfile);
+  //   expect(updatedProfile).toEqual({ id: 1, ...updateTestProfile });
+  //   expect(mockProfileService.updateProfile).toHaveBeenCalledWith(
+  //     1,
+  //     updateTestProfile,
+  //   );
+  // });
+
+  // it('should throw NotFoundException when updating a non-existent profile', async () => {
+  //   await expect(
+  //     controller.updateProfile(2, updateTestProfile),
+  //   ).rejects.toThrowError(NotFoundException);
+  //   expect(mockProfileService.updateProfile).toHaveBeenCalledWith(
+  //     2,
+  //     updateTestProfile,
+  //   );
+  // });
+
+  // it('should delete a profile', async () => {
+  //   const result = await controller.deleteProfile(1);
+  //   expect(result).toEqual('Profile deleted');
+  //   expect(mockProfileService.deleteProfile).toHaveBeenCalledWith(1);
+  // });
+
+  // it('should throw NotFoundException when deleting a non-existent profile', async () => {
+  //   await expect(controller.deleteProfile(2)).rejects.toThrowError(
+  //     NotFoundException,
+  //   );
+  //   expect(mockProfileService.deleteProfile).toHaveBeenCalledWith(2);
+  // });
 });
--- src/profiles/profiles.service.spec.ts
+++ src/profiles/profiles.service.spec.ts
@@ -1,18 +1,247 @@
+import { NotFoundException, ConflictException } from '@nestjs/common';
+import { getRepositoryToken } from '@nestjs/typeorm';
 import { Test, TestingModule } from '@nestjs/testing';
+import { Repository } from 'typeorm';
 import { ProfilesService } from './profiles.service';
+import { Profile } from './profile.entity';
+import { User } from '../users/entities/user.entity';
+import { CreateProfileDto } from './dto/create-profile.dto';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 describe('ProfilesService', () => {
   let service: ProfilesService;
+  let profileRepository: Repository<Profile>;
+  let userRepository: Repository<User>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ProfilesService],
+      providers: [
+        ProfilesService,
+        {
+          provide: getRepositoryToken(Profile),
+          useClass: Repository,
+        },
+        {
+          provide: getRepositoryToken(User),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<ProfilesService>(ProfilesService);
+    profileRepository = module.get<Repository<Profile>>(
+      getRepositoryToken(Profile),
+    );
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('createProfile', () => {
+    const createProfileDto: CreateProfileDto = {
+      gender: 'male',
+      occupation: 'developer',
+      phoneNumber: '123456789',
+      firstName: 'John',
+      lastName: 'Doe',
+      email: 'john.doe@example.com',
+    };
+
+    it('should create a new profile when the user exists and has no profile', async () => {
+      const user = new User();
+      user.id = 1;
+
+      userRepository.findOne = jest.fn().mockResolvedValue(user);
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(null);
+      profileRepository.create = jest.fn().mockReturnValue(createProfileDto);
+      profileRepository.save = jest.fn().mockResolvedValue(createProfileDto);
+
+      const result = await service.createProfile(createProfileDto);
+
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: createProfileDto.email },
+      });
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({ id: user.id });
+      expect(profileRepository.create).toHaveBeenCalledWith({
+        id: user.id,
+        ...createProfileDto,
+        user,
+      });
+      expect(profileRepository.save).toHaveBeenCalledWith(createProfileDto);
+      expect(result).toEqual(createProfileDto);
+    });
+
+    it('should throw NotFoundException when the user does not exist', async () => {
+      userRepository.findOne = jest.fn().mockResolvedValue(null);
+
+      await expect(
+        service.createProfile(createProfileDto),
+      ).rejects.toThrowError(NotFoundException);
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: createProfileDto.email },
+      });
+    });
+
+    it('should throw ConflictException when the user already has a profile', async () => {
+      const user = new User();
+      user.id = 1;
+
+      userRepository.findOne = jest.fn().mockResolvedValue(user);
+      profileRepository.findOneBy = jest.fn().mockResolvedValue({});
+
+      await expect(
+        service.createProfile(createProfileDto),
+      ).rejects.toThrowError(ConflictException);
+      expect(userRepository.findOne).toHaveBeenCalledWith({
+        where: { email: createProfileDto.email },
+      });
+    });
+  });
+
+  describe('getProfileById', () => {
+    const profileId = 1;
+
+    it('should return the profile with the given ID', async () => {
+      const profile = new Profile();
+      profile.id = profileId;
+
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(profile);
+
+      const result = await service.getProfileById(profileId);
+
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({
+        id: profileId,
+      });
+      expect(result).toEqual(profile);
+    });
+
+    it('should throw NotFoundException when the profile does not exist', async () => {
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(null);
+
+      await expect(service.getProfileById(profileId)).rejects.toThrowError(
+        NotFoundException,
+      );
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({
+        id: profileId,
+      });
+    });
+  });
+
+  describe('getAllProfiles', () => {
+    it('should return all profiles', async () => {
+      const user: User = {
+        id: 1,
+        userName: 'johndoe',
+        email: 'johndoe@example.com',
+        profile: null,
+        tasks: [],
+        hasId: () => true,
+        save: () => Promise.resolve(user),
+        remove: () => Promise.resolve(user),
+        softRemove: () => Promise.resolve(user),
+        recover: () => Promise.resolve(user),
+        reload: () => Promise.resolve(),
+      };
+      const profiles: Profile[] = [
+        {
+          id: 1,
+          gender: 'male',
+          occupation: 'developer',
+          firstName: 'John',
+          lastName: 'Doe',
+          phoneNumber: '123',
+          user: user,
+        },
+        {
+          id: 2,
+          gender: 'female',
+          occupation: 'designer',
+          firstName: 'Jane',
+          lastName: 'Smith',
+          phoneNumber: '456',
+          user: user,
+        },
+      ];
+
+      profileRepository.find = jest.fn().mockResolvedValue(profiles);
+
+      const result = await service.getAllProfiles();
+
+      expect(profileRepository.find).toHaveBeenCalled();
+      expect(result).toEqual(profiles);
+    });
+  });
+
+  describe('updateProfile', () => {
+    const profileId = 1;
+    const updateProfileDto: UpdateProfileDto = {
+      occupation: 'new occupation',
+      phoneNumber: 'new phone number',
+    };
+
+    it('should update the profile with the given ID', async () => {
+      const profile = new Profile();
+      profile.id = profileId;
+
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(profile);
+      profileRepository.save = jest
+        .fn()
+        .mockResolvedValue({ ...profile, ...updateProfileDto });
+
+      const result = await service.updateProfile(profileId, updateProfileDto);
+
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({
+        id: profileId,
+      });
+      expect(profileRepository.save).toHaveBeenCalledWith({
+        id: profileId,
+        ...updateProfileDto,
+      });
+      expect(result).toEqual({ ...profile, ...updateProfileDto });
+    });
+
+    it('should throw NotFoundException when the profile does not exist', async () => {
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(null);
+
+      await expect(
+        service.updateProfile(profileId, updateProfileDto),
+      ).rejects.toThrowError(NotFoundException);
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({
+        id: profileId,
+      });
+    });
+  });
+
+  describe('deleteProfile', () => {
+    const profileId = 1;
+
+    it('should delete the profile with the given ID', async () => {
+      const profile = new Profile();
+      profile.id = profileId;
+
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(profile);
+      profileRepository.remove = jest.fn().mockResolvedValue(profile);
+
+      const result = await service.deleteProfile(profileId);
+
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({
+        id: profileId,
+      });
+      expect(profileRepository.remove).toHaveBeenCalledWith(profile);
+      expect(result).toBe('Profile deleted');
+    });
+
+    it('should throw NotFoundException when the profile does not exist', async () => {
+      profileRepository.findOneBy = jest.fn().mockResolvedValue(null);
+
+      await expect(service.deleteProfile(profileId)).rejects.toThrowError(
+        NotFoundException,
+      );
+      expect(profileRepository.findOneBy).toHaveBeenCalledWith({
+        id: profileId,
+      });
+    });
   });
 });
